d9f93cf02568e8a58987341dc491b59bf99da885
jrimadrazo <jrimadrazo@gmail.com>
2024-09-23 13:42:03 +0800

    updated connections

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 const corsOptions = {
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://blog-app-client-nu.vercel.app/'],
     credentials: true,
     optionsSuccessStatus: 200
 };
